20c51ef84f8c852e8eda67f18452b152601cf874
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-02-07 15:25:45 +0100

    1

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable} from "@angular/core";
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+//HttpInterceptor ha un intercept() metodo per ispezionare e trasformare le richieste http prima che vengano inviate al server
+//implementa HttpInterceptor e aggiungiamo withCredentials true per fa in modo che il browser includa un cookiie nell'intestazione della richiesta
+
+@Injectable()
+export class HttpRequestIntercepotor implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({withCredentials: true});
+    return next.handle(req);
+  }//intercept ottiene l'oggetto httpRequest, lo modifica e lo inoltra al metodo httpHandler.
+   //handle(req) trasforma httpRequest oggetto in un file Observable<httpEvents>, next rappresenta l'ultimo intercettore
+}
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, userClass: HttpRequestIntercepotor, multi: true},
+];
